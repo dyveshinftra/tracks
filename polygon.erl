b52294a0f348625ec0bdfe94c81d78a254da724c
straed1 <danny.dv.verstraeten@nokia.com>
2022-05-18 20:31:03 +0200

    #3 refactor

--- polygon.erl
+++ polygon.erl
@@ -1,23 +1,39 @@
 % a closed circuit
--module(polygon).
+%
+% TODOS:
+%   eunit
+%   performance improvement
 
-% TODO join_test: convert to eunit
+-module(polygon).
 -export([from_list/1, join/2, join_test/0]).
 
 from_list(L) -> {polygon, L}.
 
-% Join two polygons on common vertices; follow first polygon until a common
-% vertex is found, then follow second polygon until the first polygon is hit
-% again.
+% join two polygons which share one common vertex or two neighbouring common
+% vertices
 join({polygon, L1}, {polygon, L2}) ->
-    from_list(lists:reverse(join_p1(L1, L2, []))).
-join_p1([X|T1], [X|T2], L3) -> join_p2(T1, T2, [X|L3], X);
-join_p1([H1|T1], [H2|T2], L3) -> join_p1(T1, [H2|T2], [H1|L3]).
-join_p2(L1, [], L3, S) -> join_p3(L1, [], [S|L3]);
-join_p2([X|T1], [X], L3, _S) -> join_p3(T1, [], [X|L3]);
-join_p2([H1|T1], [H2|T2], L3, S) -> join_p2([H1|T1], T2, [H2|L3], S).
-join_p3([], [], L3) -> L3;
-join_p3([H|T], [], L3) -> join_p3(T, [], [H|L3]).
+    from_list(lists:reverse(join(L1, L2, [], first, undefined))).
+
+% both polygons consumed, all done, return accumulated polygon
+join([], [], A, _, _) ->
+    A;
+
+% consume (tail of) first polygon
+join([H|T], [], A, first, V) ->
+    join(T, [], [H|A], first, V);
+
+% single common vertex and second polygon is consumes,
+% insert common vertex again and switch back to first polygon
+join(P1, [], A, second, V) ->
+    join(P1, [], [V|A], first, V);
+
+% common vertex found, switch between polygons, remember common vertex
+join([H|T1], [H|T2], A, first, _) -> join(T1, T2, [H|A], second, H);
+join([H|T1], [H|T2], A, second, V) -> join(T1, T2, [H|A], first, V);
+
+% when polygon vertices differ, take from either first or second polygon
+join([H1|T1], [H2|T2], A, first, V) -> join(T1, [H2|T2], [H1|A], first, V);
+join([H1|T1], [H2|T2], A, second, V) -> join([H1|T1], T2, [H2|A], second, V).
 
 join_test() ->
     {polygon, [1, 2, 4, 3]} = join(from_list([1, 2, 3]), from_list([2, 4, 3])),
